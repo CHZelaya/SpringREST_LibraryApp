7b2783c3d2de60f9929562f45da2fa50b2a72a51
CHZelaya <chzelaya1201@gmail.com>
2025-03-19 17:54:42 -0600

    First commit

---
+++ src/main/java/oosd/sait/springrest_libraryapp/SpringRestLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package oosd.sait.springrest_libraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestLibraryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestLibraryAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oosd/sait/springrest_libraryapp/SpringRestLibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package oosd.sait.springrest_libraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestLibraryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
