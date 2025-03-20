18ba4de9fa8851b71e6c6b24a7d9742f7d11cde0
CHZelaya <chzelaya1201@gmail.com>
2025-03-20 17:54:41 -0600

    [feat] Added Entity Author and finished DB mapping with Entity.Book

--- src/main/java/oosd/sait/springrest_libraryapp/entities/Author.java
+++ src/main/java/oosd/sait/springrest_libraryapp/entities/Author.java
@@ -1,8 +1,10 @@
 package oosd.sait.springrest_libraryapp.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 @Entity
 @Table(name="author")
@@ -10,11 +12,62 @@ public class Author {
     @Id
     private Long id;
 
+    @NotNull
+    String name;
+
+    String biography;
+
+    @ManyToMany
+    private List<Book> books;
+
+    public Author(String name, String biography, List<Book> books) {
+        this.name = name;
+        this.biography = biography;
+        this.books = books;
+    }
+
+    public Author() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
 
     public Long getId() {
         return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", biography='" + biography + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
